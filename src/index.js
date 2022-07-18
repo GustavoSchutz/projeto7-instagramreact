1b6f17b65b19ebe7e606364a6a59b28434cf3204
GustavoSchutz <gutofelipe8@gmail.com>
2022-07-18 14:49:57 -0300

    Tudo no JSX

---
+++ src/index.js
@@ -0,0 +1,308 @@
+import ReactDOM from "react-dom";
+
+function Header() {
+    
+    return (
+        <section  className="header">
+            <div  className="topo">
+                <div  className="logo">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    <img src="./images/logo.png" alt="" />
+                </div>
+                <div  className="search">
+                    <p>Pesquisar</p>
+                </div>
+                <div  className="icons">
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                    <ion-icon name="compass-outline"></ion-icon>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="person-outline"></ion-icon>
+                </div>
+
+            </div>
+        </section>
+ );
+}
+
+function Container() {
+    return (
+        <section  className="container">
+        <div  className="main">
+            <div  className="stories">
+               <div  className="padding"></div>
+               <div  className="storiebox">
+                    <div  className="storie">
+                        <img src="./images/9gag 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt="" />
+                        <div  className="storyprofile"><p>9gag</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/meowed 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>meowed</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/barked 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>barked</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/nathanwpylestrangeplanet 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>nathanwpyle...</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/wawawiwacomicsa 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>wawawiwac...</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/respondeai 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>respondeai</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/filomoderna 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>filomoderna</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/memeriagourmet 1.png" alt=""    className="inner-image" />
+                        <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                        <div  className="storyprofile"><p>memeriagourmet</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/9gag 1.png" alt=""    className="inner-image"/>
+                         <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                         <div  className="storyprofile"><p>9gag</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/9gag 1.png" alt=""    className="inner-image"/>
+                         <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                         <div  className="storyprofile"><p>9gag</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/9gag 1.png" alt=""    className="inner-image"/>
+                         <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                         <div  className="storyprofile"><p>9gag</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/9gag 1.png" alt=""    className="inner-image"/>
+                         <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                         <div  className="storyprofile"><p>9gag</p></div>
+                    </div>
+                    <div  className="storie">
+                         <img src="./images/9gag 1.png" alt=""    className="inner-image"/>
+                         <img src="./images/stories_background.jpg"  className="top" alt=""/>
+                         <div  className="storyprofile"><p>9gag</p></div>
+                    </div>
+                    <ion-icon name="chevron-forward-circle-outline"></ion-icon>
+               </div>
+            
+            
+            </div>
+            <div  className="feed">
+                <div  className="post">
+                    <div  className="profile">
+                        <div  className="profile-pic"><img src="./images/meowed 2.png" alt=""/></div>
+                        <p>meowed</p>
+                    </div>
+                    <div  className="post-photo"><img src="./images/gato-telefone 1.png" alt=""/></div>
+                    <div  className="post-reactions">
+                        <div  className="reaction-icons">
+                            <div  className="like-comment-share">
+                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="chatbubble-outline"></ion-icon>
+                                <ion-icon name="paper-plane-outline"></ion-icon>
+                            </div>
+                            <div  className="save-post">
+                                <ion-icon name="bookmark-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div  className="likes">
+                            <img src="./images/respondeai 2.png" alt=""/>
+                            <p>Curtido por<strong  className="medium"> respondeai </strong> e <strong  className="medium">outras 101.523 pessoas</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div  className="post">
+                    <div  className="profile">
+                        <div  className="profile-pic"><img src="./images/meowed 2.png" alt=""/></div>
+                        <p>meowed</p>
+                    </div>
+                    <div  className="post-photo"><img src="./images/gato-telefone 1.png" alt=""/></div>
+                    <div  className="post-reactions">
+                        <div  className="reaction-icons">
+                            <div  className="like-comment-share">
+                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="chatbubble-outline"></ion-icon>
+                                <ion-icon name="paper-plane-outline"></ion-icon>
+                            </div>
+                            <div  className="save-post">
+                                <ion-icon name="bookmark-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div  className="likes">
+                            <img src="./images/respondeai 2.png" alt=""/>
+                            <p>Curtido por<strong  className="medium"> respondeai </strong> e <strong  className="medium">outras 101.523 pessoas</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div  className="post">
+                    <div  className="profile">
+                        <div  className="profile-pic"><img src="./images/meowed 2.png" alt=""/></div>
+                        <p>meowed</p>
+                    </div>
+                    <div  className="post-photo"><img src="./images/gato-telefone 1.png" alt=""/></div>
+                    <div  className="post-reactions">
+                        <div  className="reaction-icons">
+                            <div  className="like-comment-share">
+                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="chatbubble-outline"></ion-icon>
+                                <ion-icon name="paper-plane-outline"></ion-icon>
+                            </div>
+                            <div  className="save-post">
+                                <ion-icon name="bookmark-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div  className="likes">
+                            <img src="./images/respondeai 2.png" alt=""/>
+                            <p>Curtido por<strong  className="medium"> respondeai </strong> e <strong  className="medium">outras 101.523 pessoas</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div  className="post">
+                    <div  className="profile">
+                        <div  className="profile-pic"><img src="./images/meowed 2.png" alt=""/></div>
+                        <p>meowed</p>
+                    </div>
+                    <div  className="post-photo"><img src="./images/gato-telefone 1.png" alt=""/></div>
+                    <div  className="post-reactions">
+                        <div  className="reaction-icons">
+                            <div  className="like-comment-share">
+                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="chatbubble-outline"></ion-icon>
+                                <ion-icon name="paper-plane-outline"></ion-icon>
+                            </div>
+                            <div  className="save-post">
+                                <ion-icon name="bookmark-outline"></ion-icon>
+                            </div>
+                        </div>
+                        <div  className="likes">
+                            <img src="./images/respondeai 2.png" alt=""/>
+                            <p>Curtido por<strong  className="medium"> respondeai </strong> e <strong  className="medium">outras 101.523 pessoas</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div  className="sidebar">
+            <div  className="sbProfile">
+                <img src="./images/sidebar/catanacomics 1.png" alt=""/>
+                <div  className="sbProfileName">
+                    <h1  className="weightMedium">catanacomics</h1>
+                    <p  className="weightLight size14 color8E8E8E">Catana</p>
+                </div>
+            </div>
+            <div  className="sbdescription">
+                <p  className="weightMedium color8E8E8E size12">Sugestôes para você</p>
+                <p  className="weightMedium color262626">Ver tudo</p>
+            </div>
+            <div  className="sbsugestions">
+                <div  className="sbsugestion">
+                    <div  className="sbsugestionsphoto"><img src="./images/sidebar/badvibesmemes 1.png" alt=""/></div>
+                    <div  className="sbsugestionsProfile">
+                        <h1  className="weightMedium size14">bad.vibes.memes</h1>
+                        <p  className="color8E8E8E size12">Segue você</p>
+                    </div>
+                    <div  className="paddingseguir"></div>
+                    <div  className="sbsugestionsFollow">
+                        <p  className="weightMedium size12 color139EF2">Seguir</p>
+                    </div>                    
+                </div>
+                <div  className="sbsugestion">
+                    <div  className="sbsugestionsphoto"><img src="./images/sidebar/badvibesmemes 1.png" alt=""/></div>
+                    <div  className="sbsugestionsProfile">
+                        <h1  className="weightMedium size14">bad.vibes.memes</h1>
+                        <p  className="color8E8E8E size12">Segue você</p>
+                    </div>
+                    <div  className="paddingseguir"></div>
+                    <div  className="sbsugestionsFollow">
+                        <p  className="weightMedium size12 color139EF2">Seguir</p>
+                    </div>                    
+                </div>
+                <div  className="sbsugestion">
+                    <div  className="sbsugestionsphoto"><img src="./images/sidebar/badvibesmemes 1.png" alt=""/></div>
+                    <div  className="sbsugestionsProfile">
+                        <h1  className="weightMedium size14">bad.vibes.memes</h1>
+                        <p  className="color8E8E8E size12">Segue você</p>
+                    </div>
+                    <div  className="paddingseguir"></div>
+                    <div  className="sbsugestionsFollow">
+                        <p  className="weightMedium size12 color139EF2">Seguir</p>
+                    </div>                    
+                </div>
+                <div  className="sbsugestion">
+                    <div  className="sbsugestionsphoto"><img src="./images/sidebar/badvibesmemes 1.png" alt=""/></div>
+                    <div  className="sbsugestionsProfile">
+                        <h1  className="weightMedium size14">bad.vibes.memes</h1>
+                        <p  className="color8E8E8E size12">Segue você</p>
+                    </div>
+                    <div  className="paddingseguir"></div>
+                    <div  className="sbsugestionsFollow">
+                        <p  className="weightMedium size12 color139EF2">Seguir</p>
+                    </div>                    
+                </div>
+                <div  className="sbsugestion">
+                    <div  className="sbsugestionsphoto"><img src="./images/sidebar/badvibesmemes 1.png" alt=""/></div>
+                    <div  className="sbsugestionsProfile">
+                        <h1  className="weightMedium size14">bad.vibes.memes</h1>
+                        <p  className="color8E8E8E size12">Segue você</p>
+                    </div>
+                    <div  className="paddingseguir"></div>
+                    <div  className="sbsugestionsFollow">
+                        <p  className="weightMedium size12 color139EF2">Seguir</p>
+                    </div>                    
+                </div>
+            </div>
+            <div  className="sbabout">
+                <p  className="thin weightLight colorC7C7C7 size11">Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade •
+                    Termos • Localizações • Contas mais relevantes • Hashtags • 
+                    Idioma</p>
+                <p  className="thin"></p>
+            </div>
+        </div>
+    </section>
+    );
+}
+
+function Bottombar() {
+    return (
+        <div  className="bottombar">
+            <div  className="bottom">
+                <div  className="icons">
+                    <ion-icon name="home"></ion-icon>
+                    <ion-icon name="search-outline"></ion-icon>
+                    <ion-icon name="add-circle-outline"></ion-icon>
+                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon name="person-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+function App() {
+    return (
+      <div>
+        <Header />
+        <Container />
+        <Bottombar />
+      </div>
+    );
+  }
+
+ReactDOM.render(<App />, document.querySelector(".root"));
