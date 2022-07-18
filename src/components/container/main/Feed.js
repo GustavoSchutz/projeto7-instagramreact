6c9fae00e4e248b13ae04bc085510afbcd052d6b
GustavoSchutz <gutofelipe8@gmail.com>
2022-07-18 18:17:12 -0300

    Agora o bug do botão sumir tá resolvido

--- src/components/container/main/Feed.js
+++ src/components/container/main/Feed.js
@@ -1,4 +1,12 @@
+
+import React from "react";
+
 export default function Feed() {
+
+    const [contador, setContador] = React.useState(101523);
+    const [corBotao, setCorBotao] = React.useState("visible");
+    const [formatoBotao, setFormatoBotao] = React.useState("heart-outline");
+
     return (
         <div  className="feed">
             <div  className="post">
@@ -10,7 +18,18 @@ export default function Feed() {
                 <div  className="post-reactions">
                     <div  className="reaction-icons">
                         <div  className="like-comment-share">
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon onClick={() => {
+                                if (corBotao === "visible") {
+                                setContador(contador + 1);
+                                setCorBotao("red");
+                                setFormatoBotao("heart");
+                                } else {
+                                    setContador(contador - 1);
+                                    setCorBotao("visible");
+                                    setFormatoBotao("heart-outline")
+                                }
+                            }} 
+                                name={formatoBotao} class={corBotao} ></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
@@ -20,7 +39,7 @@ export default function Feed() {
                     </div>
                     <div  className="likes">
                         <img src="./images/respondeai 2.png" alt=""/>
-                        <p>Curtido por<strong  className="medium"> respondeai </strong> e <strong  className="medium">outras 101.523 pessoas</strong></p>
+                        <p>Curtido por<strong  className="medium"> respondeai </strong> e <strong  className="medium">outras {contador} pessoas</strong></p>
                     </div>
                 </div>
             </div>
